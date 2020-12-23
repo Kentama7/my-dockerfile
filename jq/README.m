3efe4045abebc1e38e4c64cce198c7ef3e279fe2
Kentama7 <kentama.7m@gmail.com>
2020-12-23 09:23:30 +0900

    Create README.m

---
+++ jq/README.m
@@ -0,0 +1,31 @@
+# jq
+
+## Version
+
+- jq: latest
+- alpine: 3.12.3
+
+## Setup
+
+`docker build -t jq .`
+
+## Usage
+
+`docker run --rm -i jq <command>`
+
+```
+echo '{"foo": 0}' | docker run --rm -i jq .
+{
+  "foo": 0
+}
+```
+
+
+### zsh
+
+```
+vi ~/.zshrc
+
+# 以下を追記
+alias jq="docker run --rm -i jq "
+```
